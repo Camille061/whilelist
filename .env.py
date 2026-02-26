8b565e41f831caf55918ceb7b10db1fe817da014
Camille061 <slideshort72@gmail.com>
2026-02-26 10:56:25 +0700

    first commit

--- .env.py
+++ .env.py
@@ -1,6 +1,1 @@
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Camille061/whilelist.git
-git push -u origin main
+adminPassword = 121221
